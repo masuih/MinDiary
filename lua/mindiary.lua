b5f7290d65fa27601aff86cac33def87a0995f98
増井 浩 <hiroshi.masui@nipponexpress.com>
2025-01-29 15:47:21 +0900

    ・今日の日記ページの実装 ・日記を保存するディレクトリをオプションで指定可能に

--- lua/mindiary.lua
+++ lua/mindiary.lua
@@ -1,21 +1,28 @@
 local M = {}
 
 M.config = {
-	message = "hello world",
+	diary_save_dir = "~",
 }
+
+local function today()
+	vim.fn.mkdir(M.config.diary_save_dir, "p")
+	vim.api.nvim_set_current_dir(M.config.diary_save_dir)
+
+	local diarybuf = vim.api.nvim_create_buf(true, false)
+	vim.api.nvim_set_current_buf(diarybuf)
+
+	local todayfile = os.date("%Y%m%d") .. ".txt"
+
+	vim.cmd("e " .. todayfile)
+
+	-- vim.api.nvim_buf_set_name(diarybuf, todaystr)
+	-- vim.api.nvim_buf_set_lines(diarybuf, 0, -1, false, { "aaaa", "bbb" })
+end
 
 M.setup = function(args)
 	M.config = vim.tbl_deep_extend("force", M.config, args or {})
 
-	vim.api.nvim_create_user_command("HelloMessage", function()
-		print(M.config.message)
-	end, {})
-
-	vim.api.nvim_create_user_command("DiaryBufferCreate", function()
-		local diarybuf = vim.api.nvim_create_buf(true, true)
-		vim.api.nvim_buf_set_name(diarybuf, "Diary")
-		vim.api.nvim_buf_set_lines(diarybuf, 0, -1, false, { "aaaa", "bbb" })
-	end, {})
+	vim.api.nvim_create_user_command("MinDiaryToday", today, {})
 end
 
 return M
